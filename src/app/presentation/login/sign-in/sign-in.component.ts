2f9f8aa97d039fbd0493c44b320aaad2ac59d969
ERuiz <edwar_775@hotmail.com>
2023-04-25 09:55:39 -0500

    Lógica componentes questions
    Se generan algunos ajustes a los componentes para usar la lógica de lectura de questions.

--- src/app/presentation/login/sign-in/sign-in.component.ts
+++ src/app/presentation/login/sign-in/sign-in.component.ts
@@ -8,58 +8,62 @@ import { NotificationService } from '../../shared/service/notification.service';
 @Component({
   selector: 'app-sign-in',
   templateUrl: './sign-in.component.html',
-  styleUrls: ['./sign-in.component.scss']
+  styleUrls: ['./sign-in.component.scss'],
 })
 export class SignInComponent implements OnInit {
-
-
   imageUrls = [
     'https://i.pinimg.com/564x/5a/8d/83/5a8d83b0efff854caaee04b56c5f8bcc.jpg',
   ];
 
-  constructor( private auth: Auth,
+  constructor(
+    private auth: Auth,
     private signInUseCase: SignInUseCase,
-    public switchUseCase : SwitchUseCase,
-    private readonly notificationService: NotificationService) { }
+    public switchUseCase: SwitchUseCase,
+    private readonly notificationService: NotificationService
+  ) {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   loginWithGoogle() {
-    return signInWithPopup(this.auth, new GoogleAuthProvider);
+    return signInWithPopup(this.auth, new GoogleAuthProvider());
   }
 
-  handlerSuccess(){
+  handlerSuccess() {
     this.notificationService.showMessage(
-      "Token sent!",
-      "Token sent, check your mail!",
-      "success"
-    )
+      'Token sent!',
+      'Token sent, check your mail!',
+      'success'
+    );
   }
 
-  handlerError(error : string){
+  handlerError(error: string) {
     this.notificationService.showMessage(
-      "Something went wrong!",
+      'Something went wrong!',
       error,
-      "error"
-    )
+      'error'
+    );
   }
-  
+
   async onclickLogin() {
     const user = await this.loginWithGoogle();
-    
-    this.signInUseCase.generateTest(user.user.email??"").subscribe({
-      next: (data : any) => {if(data.success){this.handlerSuccess()} else {this.handlerError(data.message)}},
-      error: (message : HttpErrorResponse) => {this.handlerError(message.error.message)},
-      
+
+    this.signInUseCase.generateTest(user.user.email ?? '').subscribe({
+      next: (data: any) => {
+        if (data.success) {
+          this.handlerSuccess();
+        } else {
+          this.handlerError(data.message);
+        }
+      },
+      error: (message: HttpErrorResponse) => {
+        this.handlerError(message.error.message);
+      },
     });
-  
+
     const token = await user.user.getIdToken();
     localStorage.setItem('token', token);
     console.log(token);
     this.switchUseCase.switchLogIn = false;
-    this.switchUseCase.switchPresentation = true; 
-
+    this.switchUseCase.switchPresentation = true;
   }
-
 }
